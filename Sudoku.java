527c7cd488088f47da516e161bfd02e095b1616e
Felicia Liu <felicia@felicialiu.com>
2014-03-19 16:45:42 +0100

    Sudoku now takes in arguments

--- Sudoku.java
+++ Sudoku.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+import java.util.regex.Pattern;
 
 public class Sudoku {
 
@@ -14,15 +15,30 @@ public class Sudoku {
 	/* alle sudoku's inlezen en totaalscore printen */
 
 	public static void main(String[] args)
-	{
+	{	
 		int total = 0;
-		int totalnot = 0;
+		int totalSolved = 0;
+		int totalUnsolved = 0;
+		String fileName = "";
+
+		if(args.length == 0){
+            total = 100;
+            fileName = "subig20.txt";
+        } else {
+        	if(args[0].equals("-f")) {
+        		fileName = args[1];
+        		total = Integer.parseInt(args[2]);
+        	} else if(!Pattern.matches("[a-zA-Z]+", args[0])){
+        		total = Integer.parseInt(args[0]);
+        		fileName = "subig20.txt";
+        	}
+        }
 		try{
-			sudokuReader = new BufferedReader(new FileReader("subig20.txt"));
+			sudokuReader = new BufferedReader(new FileReader(fileName));
 		} catch (IOException e) {
-	            e.printStackTrace();
-	        }
-		for (int i = 0; i < 100 ;i++) {
+	        e.printStackTrace();
+	    }
+		for (int i = 0; i < total ;i++) {
 			Board test = new Board(sudokuReader);
 			/*
 			drawing = new SudokuGraphics(9,9,test);
@@ -39,14 +55,17 @@ public class Sudoku {
 			//drawSudoku(9,9, test);
 			//System.out.println("Sudoku "+i+" solved is "+solved);
 			if(solved == true){
-				total++;
+				totalSolved++;
 				drawSudoku(9,9,test);
-			}else{
-				totalnot++;
+			} else {
+				totalUnsolved++;
+				drawSudoku(9,9,test);
 			}
 		}
-		System.out.println("total solved "+total);
-		System.out.println("total not solved "+totalnot);
+
+		System.out.println("Total solved " + totalSolved);
+		System.out.println("total unsolved " + totalUnsolved);
+		System.out.println("Percentage of correctly solved sudoku's is " + (totalSolved/(total+0.0))*100);
 		/*
 		for (int i =0; i<9 ;i++ ) {
 			Hidden.hiddenpair(test.getColumn(i));	
@@ -103,7 +122,7 @@ public class Sudoku {
 			int rowIndex = -1;
 			int columnIndex = 0;
 			int blockIndex = 0;		
-			
+			initBoard(board);
 			
 			// Checks all 81 objects if one of the values can be assigned
 			for(int count = 0; count < 81; count++) {
@@ -136,7 +155,8 @@ public class Sudoku {
 				columnIndex++;
 			}
 			if(same){
-				//NakedPairs.nakedPairs(board);
+				NakedPairs.nakedPairs(board);
+				initBoard(board);
 				/*
 				for (int i =0; i<9 ;i++ ) {
 					
